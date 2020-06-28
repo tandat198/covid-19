52834881d7b112894b584bb3e83e8cb40a8bceff
Dat Nguyen <51848355+tandat198@users.noreply.github.com>
2020-06-28 20:10:30 +0700

    Update index.js

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loader = (props) => (
-    <svg style={{ width: "10%" }} viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'>
+    <svg style={{ height: "10%" }} viewBox='0 0 100 100' preserveAspectRatio='xMidYMid'>
         <path d='M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50' fill={props.fill ? props.fill : "#3e8ef7"} stroke='none' transform='rotate(53.2058 50 51)'>
             <animateTransform attributeName='transform' type='rotate' dur='1s' repeatCount='indefinite' keyTimes='0;1' values='0 50 51;360 50 51'></animateTransform>
         </path>
